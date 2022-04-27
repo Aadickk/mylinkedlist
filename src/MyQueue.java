845f438736ade8d43c1a17c5bfcd69910c60c323
Aadickk <adik54112@gmail.com>
2022-04-28 00:37:02 +0600

    Added MyQueue and MyHeap

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,2 +1,19 @@
-public class MyQueue {
+public class MyQueue <T extends Comparable<T>> {
+    MyArrayList<T> queue = new MyArrayList<>();
+    int size = 0;
+
+    void push(T data) {
+        queue.add(data);
+        size++;
+    }
+
+    T peek() {
+        return queue.get(0);
+    }
+
+    T pop() {
+        T h = queue.get(0);
+        queue.remove(0);
+        return h;
+    }
 }
